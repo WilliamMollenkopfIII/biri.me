cbb8093d8acb8075af151a278b4806ae88260c84
wmollenkopf <biri@biri.me>
2018-08-02 19:54:06 -0400

    This is the first basic site structure. This is pretty solid so far I think, and will expand more and more from here.

---
+++ js/script.js
@@ -0,0 +1,37 @@
+var lnStickyNavigation;
+
+$(document).ready(function(){	
+	fixNav();
+	//applyHeader();
+	//applyNavigation(); 
+	//applyMailTo();
+	//applyResize();
+	//checkHash();
+	//checkBrowser();
+});
+
+/* HEADER FUNCTIONS */
+
+	// fix the header padding issues with it overlaying on top of the body
+	// If it doesn't work: try these: https://stackoverflow.com/questions/11124777/twitter-bootstrap-navbar-fixed-top-overlapping-site
+	// It should constantly resize the padding of the area to match the window size properly.
+
+
+
+   function fixNav(){
+   		// fix the header padding issues with it overlaying on top of the body
+		// If it doesn't work: try these: https://stackoverflow.com/questions/11124777/twitter-bootstrap-navbar-fixed-top-overlapping-site
+		// It should constantly resize the padding of the area to match the window size properly.
+        $(document.body).css('padding-top', $('#topnavbar').height() + 0);
+        $(window).resize(function(){
+            $(document.body).css('padding-top', $('#topnavbar').height() + 0);
+        });
+		
+		
+
+   }
+/*
+function applyHeader()
+{
+	$('.jumbotron').css({ height: ($(window).height()/2) +'px' });
+} */
