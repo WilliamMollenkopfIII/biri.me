6000d99ffcd1baccf268d09f45d6be3a219e1c75
wmollenkopf <biri@biri.me>
2018-12-19 12:14:17 -0500

    Linted script.js and slight page edit.
    Linted the script.js and slightly edited the about me section.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 var lnStickyNavigation;
 
-$(document).ready(function() {	
+$(document).ready(function () {
 	//fixNav();
 	applyHeader();
 	applyNavigation();
@@ -12,29 +12,29 @@ $(document).ready(function() {
 
 /* HEADER FUNCTIONS */
 
+// fix the header padding issues with it overlaying on top of the body
+// If it doesn't work: try these: https://stackoverflow.com/questions/11124777/twitter-bootstrap-navbar-fixed-top-overlapping-site
+// It should constantly resize the padding of the area to match the window size properly.
+
+
+
+function fixNav() {
 	// fix the header padding issues with it overlaying on top of the body
 	// If it doesn't work: try these: https://stackoverflow.com/questions/11124777/twitter-bootstrap-navbar-fixed-top-overlapping-site
 	// It should constantly resize the padding of the area to match the window size properly.
+	$(document.body).css('padding-top', $('#topnavbar').height() + 0);
+	$(window).resize(function () {
+		$(document.body).css('padding-top', $('#topnavbar').height() + 0);
+	});
 
 
 
-   function fixNav() {
-   		// fix the header padding issues with it overlaying on top of the body
-		// If it doesn't work: try these: https://stackoverflow.com/questions/11124777/twitter-bootstrap-navbar-fixed-top-overlapping-site
-		// It should constantly resize the padding of the area to match the window size properly.
-        $(document.body).css('padding-top', $('#topnavbar').height() + 0);
-        $(window).resize(function(){
-            $(document.body).css('padding-top', $('#topnavbar').height() + 0);
-        });
-		
-		
-
-   }
+}
 
 /* HEADER FUNCTIONS */
 function applyHeader() {
-	$('.jumbotron').css({ height: ($(window).height()) +'px' });
-} 
+	$('.jumbotron').css({ height: ($(window).height()) + 'px' });
+}
 
 
 
@@ -48,15 +48,15 @@ function applyNavigation() {
 }
 
 function applyClickEvent() {
-	$('a[href*=#]').on('click', function(e) {
+	$('a[href*=#]').on('click', function (e) {
 		e.preventDefault();
 		let anchor = $.attr(this, 'href');
-		if( $(anchor).length > 0 ) {
-			if(anchor=='#cv') {
-				$('html, body').animate({scrollTop: $(anchor).offset().top}, 400).then($('#past-projects').modal('show'));
+		if ($(anchor).length > 0) {
+			if (anchor == '#cv') {
+				$('html, body').animate({ scrollTop: $(anchor).offset().top }, 400).then($('#past-projects').modal('show'));
 			}
 			else {
-				$('html, body').animate({scrollTop: $(anchor).offset().top}, 400);
+				$('html, body').animate({ scrollTop: $(anchor).offset().top }, 400);
 			}
 		}
 		return false;
@@ -64,41 +64,41 @@ function applyClickEvent() {
 }
 
 function applyNavigationFixForPhone() {
-	$('.navbar li a').click(function(event)  {
+	$('.navbar li a').click(function (event) {
 		$('.navbar-collapse').removeClass('in').addClass('collapse');
 	});
 }
 
 function applyScrollSpy() {
-	$('#navbar-example').on('activate.bs.scrollspy', function() {
+	$('#navbar-example').on('activate.bs.scrollspy', function () {
 		window.location.hash = $('.nav .active a').attr('href').replace('#', '#/');
 	});
 }
 
 function applyStickyNavigation() {
 	lnStickyNavigation = $('.scroll-down').offset().top + 20;
-	
-	$(window).on('scroll', function() {  
-		stickyNavigation();  
-	});  
-	
+
+	$(window).on('scroll', function () {
+		stickyNavigation();
+	});
+
 	stickyNavigation();
 }
 
-function stickyNavigation() {         
-	if($(window).scrollTop() > lnStickyNavigation) 	{   
-		$('body').addClass('fixed');  
-	} 
-	else {  
-		$('body').removeClass('fixed');   
-	}  
+function stickyNavigation() {
+	if ($(window).scrollTop() > lnStickyNavigation) {
+		$('body').addClass('fixed');
+	}
+	else {
+		$('body').removeClass('fixed');
+	}
 }
 
 /* MAILTO FUNCTION */
 function applyMailTo() {
 	// Unreverses the e-mail address so that only when clicked by the client will it render with the correct mailto:
 	// This is to make it harder for bots to render/parse the source for my e-mail.
-	$('a[href*=mailto]').on('click', function(e) {
+	$('a[href*=mailto]').on('click', function (e) {
 		var lstrEmail = $(this).attr('href').replace('mailto:', '');
 		lstrEmail = lstrEmail.split('').reverse().join('')
 		$(this).attr('href', 'mailto:' + lstrEmail);
@@ -108,18 +108,18 @@ function applyMailTo() {
 
 /* RESIZE FUNCTION */
 function applyResize() {
-	$(window).on('resize', function() 	{  
+	$(window).on('resize', function () {
 		lnStickyNavigation = $('.scroll-down').offset().top + 20;
-		$('.jumbotron').css({ height: ($(window).height()) +'px' });
-	}); 
+		$('.jumbotron').css({ height: ($(window).height()) + 'px' });
+	});
 }
 
 /* HASH FUNCTION */
 
 function checkHash() {
 	lstrHash = window.location.hash.replace('#/', '#');
-	if($('a[href='+ lstrHash +']').length > 0) {
-		$('a[href='+ lstrHash +']').trigger('click');
+	if ($('a[href=' + lstrHash + ']').length > 0) {
+		$('a[href=' + lstrHash + ']').trigger('click');
 	}
 
 }
@@ -128,8 +128,8 @@ function checkHash() {
 
 function checkBrowser() {
 	var loBrowserVersion = getBrowserAndVersion();
-	
-	if(loBrowserVersion.browser == 'Explorer' && loBrowserVersion.version < 8) 	{ 
+
+	if (loBrowserVersion.browser == 'Explorer' && loBrowserVersion.version < 8) {
 		$('#upgrade-dialog').modal({
 			backdrop: 'static',
 			keyboard: false
@@ -139,12 +139,12 @@ function checkBrowser() {
 
 function getBrowserAndVersion() {
 	var laBrowserData = [{
-		string: 		navigator.userAgent,
-		subString: 		'MSIE',
-		identity: 		'Explorer',
-		versionSearch: 	'MSIE'
+		string: navigator.userAgent,
+		subString: 'MSIE',
+		identity: 'Explorer',
+		versionSearch: 'MSIE'
 	}];
-	
+
 	return {
 		browser: searchString(laBrowserData) || 'Modern Browser',
 		version: searchVersion(navigator.userAgent) || searchVersion(navigator.appVersion) || '0.0'
@@ -152,29 +152,29 @@ function getBrowserAndVersion() {
 }
 
 function searchString(paData) {
-	for(var i = 0; i < paData.length; i++) {
-		var lstrDataString 	= paData[i].string;
-		var lstrDataProp 	= paData[i].prop;
-		
+	for (var i = 0; i < paData.length; i++) {
+		var lstrDataString = paData[i].string;
+		var lstrDataProp = paData[i].prop;
+
 		this.versionSearchString = paData[i].versionSearch || paData[i].identity;
-		
-		if(lstrDataString) {
-			if(lstrDataString.indexOf(paData[i].subString) != -1) {
+
+		if (lstrDataString) {
+			if (lstrDataString.indexOf(paData[i].subString) != -1) {
 				return paData[i].identity;
 			}
 		}
-		else if(lstrDataProp) {
+		else if (lstrDataProp) {
 			return paData[i].identity;
 		}
 	}
 }
-	
+
 function searchVersion(pstrDataString) {
 	var lnIndex = pstrDataString.indexOf(this.versionSearchString);
-	
-	if(lnIndex == -1) {
+
+	if (lnIndex == -1) {
 		return;
 	}
-	
+
 	return parseFloat(pstrDataString.substring(lnIndex + this.versionSearchString.length + 1));
 }
